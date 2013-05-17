32e7a6b0bee1769a5426add781d41fb7114ede63
Paul Dawson <excidere@gmail.com>
2013-05-17 08:09:55 -0500

    building a test setup

--- lib/jepar-b/db_manager.rb
+++ lib/jepar-b/db_manager.rb
@@ -5,7 +5,7 @@ module JeparB
     def initialize(name)
       @connection = PG.connect(dbname: name)
     end
-    attr_reader :connection
+    attr_accessor :connection
 
     def add(category, value, answer, question)
       @connection.exec("INSERT INTO answers (category, value, answer, question) VALUES ($1, $2, $3, $4);", [category, value, answer, question])
@@ -17,6 +17,18 @@ module JeparB
         answers[row["answer"]] = row["question"]
       end
       answers
+    end
+
+    def get_categories
+      @connection.exec("SELECT LOWER(category) FROM answers GROUP BY LOWER(category);").each do |row|
+        yield row["lower"]
+      end
+    end
+
+    def get_answers_from_category(category)
+      @connection.exec("SELECT value, answer, question FROM answers WHILE LOWER(category) = LOWER($1);", [category]).each do |answer|
+        yield answer
+      end
     end
   end
 end
--- setup_test_db.rb
+++ setup_test_db.rb
@@ -1,10 +1,19 @@
 require 'pg'
+require_relative "lib/jepar-b/db_manager"
+
 system("dropdb jeparb_test")
 system("createdb jeparb_test")
 
-db = PG.connect(dbname: "jeparb_test")
+@db = JeparB::DBManager.new("jeparb_test")
 
-db.exec("CREATE TABLE answers (category VARCHAR, value VARCHAR, question VARCHAR, answer VARCHAR);")
+@db.connection.exec("CREATE TABLE answers (category VARCHAR, value VARCHAR, question VARCHAR, answer VARCHAR);")
 
+@db.add("fruit", "200", "It is red and sometimes crisp.", "What is an apple?")
+@db.add("fruit", "400", "Brightly colored and a kind of tart.", "What is an orange?")
+@db.add("dinner", "200", "It's what's for dinner", "beef")
+@db.add("Dinner", "400", "The other white meat", "pork")
 
+@db.get_categories do |category|
+  puts category
+end
 
